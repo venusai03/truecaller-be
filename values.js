bf205771dd5aa3e24c654a18140a41e85c9c3918
Venu Sai Madisetti <venu@betacrew.io>
2023-11-26 18:31:25 +0530

    initial commit

--- values.js
+++ values.js
@@ -1,8 +1,8 @@
-let PREVIOUS_REQUEST_ID;
-let REQUEST_ID;
-let ACCESS_TOKEN;
-let ENDPOINT;
-let STATUS;
+let PREVIOUS_REQUEST_ID = "";
+let REQUEST_ID = "";
+let ACCESS_TOKEN = "";
+let ENDPOINT = "";
+let STATUS = "";
 
 module.exports = {
     PREVIOUS_REQUEST_ID,
